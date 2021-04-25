4e4f900bc02cdc6779da0c69111b1eb3c834d944
wx <251318979@qq.com>
2021-04-25 16:21:09 +0800

    交换两个同样大小数组的内容

---
+++ arr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int arr1[4] = { 1,2,3,4 };
+    int arr2[4] = { 5,6,7,8 };
+    int i = 0;
+    for (; i < 4; i++) {
+        int temp;
+        temp = arr1[i];
+        arr1[i] = arr2[i];
+        arr2[i] = temp;
+    }
+    for (i = 0; i < 4; i++) {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+    for (i = 0; i < 4; i++) {
+        printf("%d ", arr2[i]);
+    }
+    return 0;
+}
